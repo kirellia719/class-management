c866de5c42af110b28f162142ddd0aa2f4f5694a
Khánh <kirellia719@gmail.com>
2025-01-03 13:23:25 +0700

    init project

--- src/page/ClassManagementPage/index.jsx
+++ src/page/ClassManagementPage/index.jsx
@@ -1,10 +1,9 @@
-import { Breadcrumb } from "rsuite";
 import "./style.scss";
 
 const ClassManagementPage = () => {
    return (
       <div className="class-management-page">
-         <Breadcrumb />
+         
       </div>
    );
 };
--- src/redux/index.js
+++ src/redux/index.js
@@ -2,11 +2,9 @@ import { combineReducers } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 
 import AuthReducer from "./AuthReducer";
-import FileManagerReducer from "./FileManagerReducer";
 
 const rootReducers = combineReducers({
    Auth: AuthReducer,
-   Files: FileManagerReducer,
 });
 
 const store = configureStore({
